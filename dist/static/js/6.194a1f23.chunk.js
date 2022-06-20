40c20f806077d1b7916f508e71b4f9e1e7279f96
muXxer <git@muxxer.de>
2022-06-20 17:21:02 +0200

    Update dashboard to support unlock condition changes

--- dist/static/js/6.194a1f23.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnode-dashboard"]=this["webpackJsonpnode-dashboard"]||[]).push([[6],{183:function(a,s,o){"use strict";o.r(s),s.default=o.p+"static/media/logo-navigation.4d21198c.svg"}}]);
-//# sourceMappingURL=6.194a1f23.chunk.js.map
